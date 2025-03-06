88d0eb43a7be8cc1c4db9226434f376e16a4f4a5
Muunatic <muunatic@typeslint.com>
2025-03-06 15:18:28 +0700

    chore: remove unnecessary

--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-import { client, token } from './src/client';
-
 import './src/client';
 import './src/structures/error';
 import './src/structures/ready';
@@ -8,5 +6,3 @@ import './src/structures/interactionCreate';
 import './src/structures/messageCreate';
 import './src/structures/voice';
 import './src/events/events';
-
-client.login(token).catch((error: Error) => console.error('\x1b[31mError\x1b[0m:', error.message));
